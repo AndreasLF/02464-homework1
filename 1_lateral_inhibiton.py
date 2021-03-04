0d17524f6305190527056c5bce0c11710e7771c9
Andreas Fiehn <andreas@lyhnefiehn.dk>
2021-03-04 17:59:30 +0100

    Fix: Ignore activation of the cells at the endpoints

--- 1_lateral_inhibiton.py
+++ 1_lateral_inhibiton.py
@@ -1,5 +1,7 @@
 def activation_level(I,w):
-    """ Calculate activation levels
+    """ Calculate activation levels. 
+    
+    The activation of the two cells at the endpoints are ignored.
 
     Args:
         I (list): Inputs from photoreceptor
@@ -10,15 +12,11 @@ def activation_level(I,w):
     """
     
     A = []
-    for index, level in enumerate(I):
-        # print(index)
-        if index == 0:
-            a = level - I[index+1]*w
-            A.append(a)
-        elif index == len(I)-1:
-            a = level - I[index-1]*w
-            A.append(a)
-        else:
+    
+    for index, _ in enumerate(I):
+        # If it is not the first and not the last element in the list
+        if index != 0 and index != len(I)-1:
+            # Calculate the activation level
             a = I[index] - w*(I[index-1]+I[index+1])
             A.append(a)
     return A
