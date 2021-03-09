62d0e3c0fb8601c7e490efbf9e3e24627cfe0abf
Andreas Fiehn <andreas@lyhnefiehn.dk>
2021-03-09 19:40:57 +0100

    Fix: Plot x-axis position

--- 2_optical_illusion.py
+++ 2_optical_illusion.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 li = __import__("1_lateral_inhibiton")
 
@@ -6,7 +7,10 @@ w = 0.1
 I = [0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5]
 
 A = li.activation_level(I,w)
-plt.plot(A)
+print(A)
+
+x = np.arange(1,len(I)-1)
 plt.plot(I)
-plt.legend(["A","I"])
+plt.plot(x,A)
+plt.legend(["Input fra fotoreceptor","Aktiveringsniveau"])
 plt.show()
